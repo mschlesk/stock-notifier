51ab0a055568217d5ece2144b02fa95c16b00d16
mschlesk <markschleske@gmail.com>
2020-10-08 01:15:14 -0600

    (feat) Take browser launch parameters from chromium

--- index.js
+++ index.js
@@ -3,5 +3,14 @@ const chromium = require('chrome-aws-lambda')
 exports.handler = async (event, context) => {
   const puppeteer = chromium.puppeteer
 
-  const browser = await puppeteer.launch()
+  const exePath = await chromium.executablePath
+
+  console.log(exePath)
+
+  const browser = await puppeteer.launch({
+    executablePath: exePath,
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    headless: chromium.headless,
+  })
 }
