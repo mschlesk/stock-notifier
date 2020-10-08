35fdc367ec818e73e8ec2dc0a2cae118fbedae5b
mschlesk <markschleske@gmail.com>
2020-10-08 01:39:15 -0600

    (feat) Quick POC script to navigate to intended URL and return a screenshot

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
-const chromium = require('chrome-aws-lambda')
+const chromium = require("chrome-aws-lambda");
+
+const targetUrl = "https://store.google.com/us/config/pixel_5";
+
+let browser, page;
 
 exports.handler = async (event, context) => {
-  const puppeteer = chromium.puppeteer
+  const puppeteer = chromium.puppeteer;
 
-  const exePath = await chromium.executablePath
+  initializePage();
 
-  console.log(exePath)
+  const screenshot = await checkStock();
 
-  const browser = await puppeteer.launch({
-    executablePath: exePath,
+  browser.close();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      buffer: screenshot,
+    }),
+  };
+};
+
+const initializePage = async () => {
+  browser = await puppeteer.launch({
+    executablePath: await chromium.executablePath,
     args: chromium.args,
     defaultViewport: chromium.defaultViewport,
     headless: chromium.headless,
-  })
-}
+  });
+
+  page = browser.newPage();
+};
+
+const checkStock = async () => {
+  await page.goto(targetUrl);
+
+  const screenshot = await page.screenshot({ encoding: "binary" });
+
+  return screenshot;
+};
