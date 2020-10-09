aad9c4a02973573c56a3071faf30d161d6532ac8
mschlesk <markschleske@gmail.com>
2020-10-08 23:09:41 -0600

    (feat) Move all config into actual config object

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const chromium = require("chrome-aws-lambda");
 
-const DEBUG = true;
+const config = {
+  targetUrl: "https://store.google.com/us/config/pixel_5",
+  targetModelName: "Sorta Sage",
+  selectors: {
+    carrierCardSelector:
+      'button[aria-label*="Google Fi (Unlocked)"].mqn-h-cards-carrier__card.ng-scope',
+    modelCardSelector: "div.mqn-lobby-swatch__card__meta",
+    modelNameSelector: "div.mqn-lobby-swatch__card__headline",
+    modelStockSelector:
+      "div.mqn-lobby-swatch__card__availability--out-of-stock",
+  },
+};
 
-const targetUrl = "https://store.google.com/us/config/pixel_5",
-  carrierCardSelector =
-    'button[aria-label*="Google Fi (Unlocked)"].mqn-h-cards-carrier__card.ng-scope',
-  modelCardSelector = "div.mqn-lobby-swatch__card__meta",
-  modelNameSelector = "div.mqn-lobby-swatch__card__headline",
-  targetModelName = "Sorta Sage",
-  modelStockSelector = "div.mqn-lobby-swatch__card__availability--out-of-stock";
-
-let browser, page, isAWS;
+let browser, page, isLocal;
 
 exports.handler = async (event) => {
-  isAWS = !!event;
+  isLocal = !event;
 
   let err,
     inStock,
@@ -52,70 +55,71 @@ const initializePage = async () => {
 };
 
 const checkStock = async () => {
-  console.log(`Navigating to page at ${targetUrl}`);
-  await page.goto(targetUrl, { waitUntil: "networkidle2" });
+  console.log(`Navigating to page at ${config.targetUrl}`);
+  await page.goto(config.targetUrl, { waitUntil: "networkidle2" });
 
   console.log("Querying page for carrier card element...");
-  const targetElement = await page.$(carrierCardSelector);
+  const targetElement = await page.$(config.selectors.carrierCardSelector);
 
   if (!targetElement)
     throw new Error(
-      `Failed to find carrier card using selector ${carrierCardSelector}`
+      `Failed to find carrier card using selector ${config.selectors.carrierCardSelector}`
     );
 
-  isAWS &&
+  isLocal &&
     (await targetElement.screenshot({
       path: "screenshots/1-carrierCard.png",
     }));
 
   // Is the carrier button disabled?
-  const carrierDisabled = await targetElement.evaluate((node) => node.disabled);
-  if (carrierDisabled) {
+  if (await targetElement.evaluate((node) => node.disabled)) {
     console.log("No stock for carrier");
     return false;
   }
 
   console.log("Stock exists for carrier, selecting carrier...");
   await targetElement.click();
-  await page.waitForSelector(modelCardSelector);
+  await page.waitForSelector(config.selectors.modelCardSelector);
   await targetElement.dispose();
 
-  isAWS &&
+  isLocal &&
     (await page.screenshot({
       path: "screenshots/2-modelsPage.png",
       fullPage: true,
     }));
 
   // Select model cards
-  const models = await page.$$(modelCardSelector);
+  const models = await page.$$(config.selectors.modelCardSelector);
   console.log(`Models found: ${models.length}`);
   if (!models.length)
     throw new Error(
-      `Failed to find model card using selector ${modelCardSelector}`
+      `Failed to find model card using selector ${config.selectors.modelCardSelector}`
     );
 
   // Filter down to "Sorta Sage" model card
   const targetModel = await models.reduce(async (acc, model) => {
     if (!!acc) return acc;
     const modelName = await model.$eval(
-      modelNameSelector,
+      config.selectors.modelNameSelector,
       (node) => node.innerText
     );
     console.log(modelName);
-    return !!modelName && modelName.includes(targetModelName) ? model : null;
+    return !!modelName && modelName.includes(config.targetModelName)
+      ? model
+      : null;
   }, null);
 
   if (!targetModel)
     throw new Error(
-      `Failed to find model matching model name ${targetModelName}`
+      `Failed to find model matching model name ${config.targetModelName}`
     );
 
-  isAWS &&
+  isLocal &&
     (await targetModel.screenshot({
       path: "screenshots/3-modelCard.png",
     }));
 
-  const inStock = await !targetModel.$(modelStockSelector);
+  const inStock = await !targetModel.$(config.selectors.modelStockSelector);
   await targetModel.dispose();
 
   if (inStock) console.log("Target model is in stock!");
